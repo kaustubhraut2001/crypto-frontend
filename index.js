5234b4a2c66a713505867e60d7744df9e51d5f20
akshaybaisware <akshaybaisware1998@gmail.com>
2024-12-17 19:12:34 +0530

    hello

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.use(bodyParser.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // Replace with your React app's URL
-    methods: ["GET", "POST", "DELETE", "UPDATE"],
+    // origin: "http://localhost:3000", // Replace with your React app's URL
+    // methods: ["GET", "POST", "DELETE", "UPDATE"],
   },
 });
 
